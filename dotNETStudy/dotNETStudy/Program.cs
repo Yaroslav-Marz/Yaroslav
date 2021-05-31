daae083eada9c5db0692acb5a5874035ac251359
Yaroslav-Marz <jrik2312@gmail.com>
2021-05-31 22:06:17 +0300

    [Task1] Add Hello World!

---
+++ dotNETStudy/dotNETStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotNETStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
